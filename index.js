883ef2c88af9b9dd38cf8b796f46d893a38f1779
skylersb <skylersb@yahoo.com>
2014-10-20 15:11:34 -0600

    not finished

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var express = require('express');
+var mongo = require('mongo');
+var Mongoose = require('mongoose');
+Mongoose.connect('mongodb://localhost/nvzgame');
+var Ninja = require('./lib/models/ninja');
+var Zombie = require('./lib/models/zombie');
+
+var app = express();
+
+var joe = new Zombie ({name: "JoeDead", health: 30, NinjasConsumed: 1});
+var timmy = new Zombie ({name: "timmy", health: 40, NinjasConsumed: 20});
+
+var ninja1 = new Ninja ({name: "unknown", health: 40, zombiesKilled: 20});
+var ninja2 = new Ninja ({name: "goodLuck", health: 50, zombiesKilled: 50});
+
+joe.save();
+timmy.save();
+ninja1.save();
+ninja2.save();
+
+
+
+console.log(joe);
+
+
+app.listen(8888);
